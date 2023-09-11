310a55be10bbaf197d65e69631dd370194ca1dff
Matiasstewart <matiasstewartusher@hotmail.com>
2023-09-11 14:52:00 -0300

    cambiando el proyecto a React con Vite

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Portfolio Matias Stewart Usher</h1>
+      <p>Coming soon...</p>
+    </>
+  )
+}
+
+export default App
